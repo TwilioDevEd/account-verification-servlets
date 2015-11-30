3a056296cb1ac13b001f7233bc2aad55f4c20b10
Agustin Camino <agustin.camino@gmail.com>
2015-11-30 09:24:52 -0500

    Add create users migration

---
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    country_code VARCHAR(4) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    authy_id INTEGER NOT NULL,
+    verified BOOLEAN NOT NULL,
+    DATE TIMESTAMP NOT NULL
+);
